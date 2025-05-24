92b756d22bb99db5d32fdb637325c0446f0c9d22
Dumass2111 <88669071+Dumass2111@users.noreply.github.com>
2025-05-24 15:24:22 +0700

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
@@ -51,7 +50,7 @@ function About() {
         </h1>
         <Google />
 
-        <Github />
+
       </Container>
     </Container>
   );
